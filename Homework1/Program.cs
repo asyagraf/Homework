25caf26396999be94cc2683cfe520b7b89b259cd
asyagraf <67205781+asyagraf@users.noreply.github.com>
2021-10-30 01:12:32 +0300

    Menu added

--- Homework1/Program.cs
+++ Homework1/Program.cs
@@ -6,6 +6,36 @@ namespace Homework1
     {
         static void Main(string[] args)
         {
+            string menu;
+            do
+            {
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.WriteLine("1 - Чтение из файла");
+                Console.WriteLine("2 - Квадратный корень");
+                Console.WriteLine("3 - HTML Яндекса");
+                Console.WriteLine("4 - Выход");
+
+                menu = Console.ReadLine();
+
+                switch(menu)
+                {
+                    case "1":
+                        //
+                        break;
+                    case "2":
+                        //
+                        break;
+                    case "3":
+                        //
+                        break;
+                    case "4":
+                        break;
+                    default:
+                        //errors
+                        break;
+                }
+            }
+            while (menu != "4");
         }
     }
 }
