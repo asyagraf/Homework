345ecdfda817028335c297420781494f33c8d305
asyagraf <67205781+asyagraf@users.noreply.github.com>
2021-11-14 10:29:04 +0300

    RepositoryTests templates

---
+++ HometaskService.UnitTests/Repositories/BookRepositoryTests.cs
@@ -0,0 +1,66 @@
+﻿using HometaskService.DBModels;
+using HometaskService.Repositories;
+using HometaskService.Repositories.Interfaces;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HometaskService.UnitTests.Repositories
+{
+    class BookRepositoryTests
+    {
+        private IBookRepository repository;
+
+        private DBBook bookToAdd;
+
+        [SetUp]
+        public void SetUp()
+        {
+            repository = new BookRepository();
+            bookToAdd = new DBBook();
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+        }
+
+        #region Get
+        [Test]
+        public void ShouldGet()
+        {
+        }
+        #endregion
+
+        #region Delete
+        [Test]
+        public void ShouldDelete()
+        {
+        }
+        #endregion
+
+        #region Create
+        [Test]
+        public void ShouldCreate()
+        {
+        }
+        #endregion
+
+        #region Update
+        [Test]
+        public void ShouldUpdate()
+        {
+        }
+        #endregion
+
+        #region GetAll
+        [Test]
+        public void ShouldGetAll()
+        {
+        }
+        #endregion
+    }
+}
---
+++ HometaskService.UnitTests/Repositories/RentalCarRepositoryTests.cs
@@ -0,0 +1,64 @@
+﻿using HometaskService.DBModels;
+using HometaskService.Models;
+using HometaskService.Repositories;
+using HometaskService.Repositories.Interfaces;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HometaskService.UnitTests.Repositories
+{
+    class RentalCarRepositoryTests
+    {
+        private IRentalRepository<DbRentalCar> repository;
+
+        [SetUp]
+        public void SetUp()
+        {
+            repository = new RentalCarRepository();
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+        }
+
+        #region Get
+        [Test]
+        public void ShouldGet()
+        {
+        }
+        #endregion
+
+        #region Delete
+        [Test]
+        public void ShouldDelete()
+        {
+        }
+        #endregion
+
+        #region Create
+        [Test]
+        public void ShouldCreate()
+        {
+        }
+        #endregion
+
+        #region Update
+        [Test]
+        public void ShouldUpdate()
+        {
+        }
+        #endregion
+
+        #region GetAll
+        [Test]
+        public void ShouldGetAll()
+        {
+        }
+        #endregion
+    }
+}
